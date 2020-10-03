25c1e4ba7109857f754f437e2ec574681bfd1a7f
Seth-MetroBots3324 <55810668+Seth-MetroBots3324@users.noreply.github.com>
2020-10-03 18:29:08 -0400

    Deleted logout function (creates problems)

--- app/src/main/java/com/example/recaicle/MainActivity.java
+++ app/src/main/java/com/example/recaicle/MainActivity.java
@@ -19,10 +19,5 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, CameraActivity.class);
         startActivity(i);
     }
-
-    public void logOut(View view) {
-        Intent i = new Intent(this, LoginScreen.class);
-        startActivity(i);
-    }
 }
 
