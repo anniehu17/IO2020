afa6a451fbb4c8abad2257d5d35858550535e758
anniehu17 <anniegracehu@gmail.com>
2020-10-03 17:59:20 -0400

    fixed camera functionality

--- app/src/main/java/com/example/recaicle/CameraActivity.java
+++ app/src/main/java/com/example/recaicle/CameraActivity.java
@@ -1,67 +1,109 @@
 package com.example.recaicle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.hardware.Camera;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
-    Camera camera; //global camera variable
-    Preview preview;
+    Uri photoURI;
+    Intent takePictureIntent;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_activity);
-        preview = new Preview(this);
-        safeCameraOpen(0);
+        imageView = (ImageView) findViewById(R.id.ImageView);
     }
 
-    private boolean safeCameraOpen(int id) {
-        boolean qOpened = false;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
-        try {
-            releaseCameraAndPreview();
-            camera = Camera.open(id);
-            qOpened = (camera != null);
-        } catch (Exception e) {
-            Log.e(getString(R.string.app_name), "failed to open Camera");
-            e.printStackTrace();
-        }
-
-        return qOpened;
-    }
-
-    private void releaseCameraAndPreview() {
-        preview.setCamera(null);
-        if (camera != null) {
-            camera.release();
-            camera = null;
-        }
-    }
-
-    public void goToHome(View view) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-    }
-
-    public void goToResults(View view) {
-        Intent i = new Intent(this, Activity3.class);
-        startActivity(i);
-    }
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    public void dispatchTakePictureIntent(View view) {
+        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         } catch (ActivityNotFoundException e) {
             // display error state to the user
         }
     }
+
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            assert extras != null;
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            // Ensure that there's a camera activity to handle the intent
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            Log.d("CameraActivity","hi0");
+            if (photoFile != null) {
+                Log.d("CameraActivity","hi2");
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.recaicle.fileprovider",
+                        photoFile);
+                Log.d("CameraActivity","hi3");
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                Log.d("CameraActivity","hi4");
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                Log.d("CameraActivity","hi5");
+            }
+        }
+    }
+
+    public void goToHome(View view) { //return button
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void goToResults(View view) { //upload button
+        Intent i = new Intent(this, Activity3.class);
+        i.putExtra("photoKey", "photoURI");
+        startActivity(i);
+    }
+
 }
+
