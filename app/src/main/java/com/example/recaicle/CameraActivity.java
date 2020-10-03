a0e383d171e259e7bf5f12ce9477f53b532b2260
anniehu17 <anniegracehu@gmail.com>
2020-10-03 14:02:38 -0400

    preview + camera

---
+++ app/src/main/java/com/example/recaicle/CameraActivity.java
@@ -0,0 +1,69 @@
+package com.example.recaicle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CameraActivity extends AppCompatActivity {
+    Camera camera; //global camera variable
+    Preview preview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+    private boolean safeCameraOpen(int id) {
+        boolean qOpened = false;
+
+        try {
+            releaseCameraAndPreview();
+            camera = Camera.open(id);
+            qOpened = (camera != null);
+        } catch (Exception e) {
+            Log.e(getString(R.string.app_name), "failed to open Camera");
+            e.printStackTrace();
+        }
+
+        return qOpened;
+    }
+
+    private void releaseCameraAndPreview() {
+        preview.setCamera(null);
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    public void goToHome(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void goToResults(View view) {
+        Intent i = new Intent(this, Activity3.class);
+        startActivity(i);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+}
--- app/src/main/java/com/example/recaicle/MainActivity.java
+++ app/src/main/java/com/example/recaicle/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToCamera(View view) {
-        Intent i = new Intent(this, Activity2.class);
+        Intent i = new Intent(this, CameraActivity.class);
         startActivity(i);
     }
 }
