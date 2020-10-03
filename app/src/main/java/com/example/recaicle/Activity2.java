45ecf3388b837849a9490d8213dc3718600ff84c
anniehu17 <anniegracehu@gmail.com>
2020-10-03 12:57:31 -0400

    made activity 3
    -fixed text on buttons
    -added activity 3 (3rd screen)

--- app/src/main/java/com/example/recaicle/Activity2.java
+++ app/src/main/java/com/example/recaicle/Activity2.java
@@ -2,7 +2,9 @@ package com.example.recaicle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void goToHome(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void goToResults(View view) {
+        Intent i = new Intent(this, Activity3.class);
+        startActivity(i);
     }
 }
