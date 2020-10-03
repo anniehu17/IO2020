93e8e57b94be72bcdb52871951fd1dbf296a7ed3
zinodyta1 <44512495+zinodyta1@users.noreply.github.com>
2020-10-03 18:30:41 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/recaicle/MainActivity.java
+++ app/src/main/java/com/example/recaicle/MainActivity.java
@@ -7,12 +7,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+
+
+        Python py = Python.getInstance();
+        
     }
 
     public void goToCamera(View view) {
